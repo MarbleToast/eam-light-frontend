c8af1753f70427ef3e8098cb50ca8e6452bba4f7
James Bradford <james.robert.bradford@cern.ch>
2021-11-15 10:22:03 +0100

    Reworked eqptree state management

--- src/ui/components/eqtree/EAMTree.js
+++ src/ui/components/eqtree/EAMTree.js
@@ -1,81 +1,99 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import TreeWS from './lib/TreeWS';
 import ErrorTypes from 'eam-components/dist/ui/components/eamgrid/lib/GridErrorTypes';
 import SortableTree from 'react-sortable-tree';
-import {addNodeUnderParent} from 'react-sortable-tree'
 import TreeTheme from './theme/TreeTheme';
 import TreeIcon from './components/TreeIcon';
-import TreeSelectParent from "./components/TreeSelectParent";
+import TreeSelectParent from './components/TreeSelectParent';
 import BlockUi from 'react-block-ui';
 
-export default function EAMTree(props) {
+const urlTypeMap = {
+    A: 'asset',
+    P: 'position',
+    S: 'system',
+    L: 'location',
+};
 
+const _getColorForType = (type) => {
+    switch (type) {
+        case 'A':
+            return '#B71C1C';
+        case 'P':
+            return '#0D47A1';
+        default:
+            return '#757575';
+    }
+};
+
+export default function EAMTree(props) {
     const [loading, setLoading] = useState(false);
-    const [treeData, setTreeData] = useState([]);
+    const [treeData, setTreeData] = useState(null);
+
+    const { code, layout, handleError } = props;
+    const { eqpTreeNewChild, eqpTreeNewRoot} = layout;
+
+    const _loadTreeData = async (code) => {
+        setLoading(true);
+        try {
+            const { body } = await TreeWS.getEquipmentStructure(code);
+            const { data } = body;
+            if (treeData) {
+                await _reExpandNodes(data);
+            }
+            setTreeData(data);
+            setLoading(false);
+        } catch (error) {
+            if (error.type !== ErrorTypes.REQUEST_CANCELLED) {
+                handleError(error);
+            }
+        }
+    };
 
     useEffect(() => {
-        _loadTreeData(props.code);
-    }, [])
+        _loadTreeData(code);
+    }, [eqpTreeNewChild, eqpTreeNewRoot]);
 
-    useEffect( () => {
-        if (props.layout.eqpTreeNewChild && treeData.length > 0) {
-            const currentNode = props.layout.eqpTreeCurrentNode ? props.layout.eqpTreeCurrentNode.id : treeData[0].id
-            setTreeData(
-                addNodeUnderParent({
-                    treeData,
-                    parentKey: currentNode,
-                    expandParent: true,
-                    newNode: props.layout.eqpTreeNewChild,
-                    getNodeKey: ({node}) => node.id
-                }).treeData
-            )
+
+    useEffect(() => {
+        _loadTreeData(code);
+    }, [code]);
+
+    const _reExpandNodes = async (newData) => {
+        if (newData && newData.length > 0) {
+            const expanded = await _getExpanded(treeData[0], []);
+            expanded.forEach((ex) => _expandTreeToCode(newData, ex));
         }
-    }, [props.layout.eqpTreeNewChild])
+    };
 
-    /**
-     * Add expanded = true for those nodes that belong to the
-     * selected system/code path in the tree
-     */
+    const _getExpanded = async (data, already) => {
+        if (data?.expanded) {
+            already.push(data.id);
+            data.children.forEach((c) => _getExpanded(c, already));
+        }
+        return already;
+    };
+
     const _expandTreeToCode = (treeData, code) => {
-        if (code && treeData && treeData.length > 0) {
-            let nodes = treeData.filter(f => (code == f.id));
+        if (!code || !treeData?.length) {
+            return false;
+        }
+        let nodes = treeData.filter((f) => code == f.id);
 
-            if (nodes.length > 0) {
-                nodes[0].expanded = true;
+        if (nodes.length > 0) {
+            nodes[0].expanded = true;
+            return true;
+        }
+
+        return treeData.some((item) => {
+            const res = _expandTreeToCode(item.children, code);
+            if (res) {
+                item.expanded = true;
                 return true;
             } else {
-                return treeData.some(item => {
-                    const res = _expandTreeToCode(item.children, code);
-                    if (res) {
-                        item.expanded = true;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
-            }
-        }
-        return false;
-    }
-
-    const _loadTreeData = (code) => {
-        setLoading(true);
-
-        TreeWS.getEquipmentStructure(code)
-            .then(data => {
-
-                // get tree data
-                let treeData = data.body.data;
-                _expandTreeToCode(treeData, props.code);
-                setTreeData(treeData);
-                setLoading(false);
-            }).catch(error => {
-            if (error.type !== ErrorTypes.REQUEST_CANCELLED) {
-                props.handleError(error);
+                return false;
             }
         });
-
-    }
+    };
 
     const _isNodeSelected = (node) => {
         if (props.layout.eqpTreeCurrentNode) {
@@ -84,59 +102,39 @@ export default function EAMTree(props) {
         return node.id === props.code;
     };
 
-    const urlTypeMap = {
-        'A': 'asset',
-        'P': 'position',
-        'S': 'system',
-        'L': 'location'
-    }
 
-    const _getColorForType = (type) => {
-        switch (type) {
-            case 'A':
-                return '#B71C1C';
-            case 'P':
-                return '#0D47A1';
-            default:
-                return '#757575';
-        }
-    }
-
-    const nodeClickHandler = rowInfo => event => {
-        if (event.target.className === `rowTitle` && ["A", "P", "S", "L"].includes(rowInfo.node.type)) {
-            if (window.location.pathname.includes("installeqp")) {
-                props.setLayoutProperty('eqpTreeCurrentNode', rowInfo.node)
+    const nodeClickHandler = (rowInfo) => (event) => {
+        if (event.target.className === `rowTitle` && ['A', 'P', 'S', 'L'].includes(rowInfo.node.type)) {
+            if (window.location.pathname.includes('installeqp')) {
+                props.setLayoutProperty('eqpTreeCurrentNode', rowInfo.node);
                 return;
             }
 
             if (props.history) {
-                props.history.push("/" + urlTypeMap[rowInfo.node.type] + `/${rowInfo.node.id}`);
+                props.history.push('/' + urlTypeMap[rowInfo.node.type] + `/${rowInfo.node.id}`);
             }
-            window.parent.postMessage(JSON.stringify({
-                type: 'EQUIPMENT_TREE_NODE_CLICK',
-                node: rowInfo.node,
-            }), "*");
+            window.parent.postMessage(
+                JSON.stringify({
+                    type: 'EQUIPMENT_TREE_NODE_CLICK',
+                    node: rowInfo.node,
+                }),
+                '*'
+            );
         }
-    }
+    };
 
     return (
-        <React.Fragment>
-            <BlockUi tag="div" blocking={loading}/>
-            <div style={{height: '100%', margin: 5}}>
-                <SortableTree
-                    canDrag={false}
-                    treeData={treeData}
-                    onChange={setTreeData}
-
-                    // this is to make the tree div height to fit the screen
-                    //style={{height: '100%'}}
-                    className='sortableTree'
-                    // set up our theme
-                    theme={TreeTheme}
-
-                    generateNodeProps={rowInfo => {
-
-                        return {
+        <>
+            <BlockUi tag="div" blocking={loading} />
+            <div style={{ height: '100%', margin: 5 }}>
+                {treeData &&
+                    <SortableTree
+                        className="sortableTree"
+                        canDrag={false}
+                        treeData={treeData}
+                        onChange={setTreeData}
+                        theme={TreeTheme}
+                        generateNodeProps={(rowInfo) => ({
                             isNodeSelected: _isNodeSelected,
                             onClick: nodeClickHandler(rowInfo),
                             icons: rowInfo.node.isDirectory
@@ -156,14 +154,15 @@ export default function EAMTree(props) {
                                     />,
                                 ]
                                 : [
-                                    <div className='selectParentButton' style={{marginLeft: '-10px'}}>
-                                        {
-                                            !rowInfo.parentNode && rowInfo.node.parents && rowInfo.node.parents.length > 0 &&
-                                            <TreeSelectParent
-                                                parents={rowInfo.node.parents}
-                                                reloadData={_loadTreeData}
-                                            />
-                                        }
+                                    <div className="selectParentButton" style={{ marginLeft: -10 }}>
+                                        {!rowInfo.parentNode &&
+                                            rowInfo.node.parents &&
+                                            rowInfo.node.parents.length > 0 && (
+                                                <TreeSelectParent
+                                                    parents={rowInfo.node.parents}
+                                                    reloadData={_loadTreeData}
+                                                />
+                                            )}
                                     </div>,
                                     <TreeIcon
                                         eqtype={rowInfo.node.type}
@@ -172,15 +171,13 @@ export default function EAMTree(props) {
                                             height: 15,
                                             marginRight: 5,
                                             marginTop: 3,
-                                            color: _getColorForType(rowInfo.node.type)
+                                            color: _getColorForType(rowInfo.node.type),
                                         }}
                                     />,
                                 ],
-                        }
-                    }
-                    }
-                />
+                        })}
+                />}
             </div>
-        </React.Fragment>
-    )
+        </>
+    );
 }
